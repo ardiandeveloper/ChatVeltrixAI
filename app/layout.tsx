ed0f4aaf1debf78fa57a85531a2bbc74d1ad1200
ardiandeveloper <ardiannjung111@gmail.com>
2025-04-28 23:10:14 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,10 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 import { getServerSideConfig } from "./config/server";
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "VeltrixAI",
+  description: "VeltrixAI Chatbot",
   appleWebApp: {
-    title: "NextChat",
+    title: "VeltrixAI",
     statusBarStyle: "default",
   },
 };
